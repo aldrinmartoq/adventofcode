7fac35f34b6a29d37d530e295a388c4d142eb44d
Aldrin Martoq <a@a0.cl>
2020-12-15 01:14:43 -0300

    Day 7.

---
+++ 007/part_1.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require 'ostruct'
+
+input_file = ARGV.first || File.join(__dir__, 'input')
+input = File.read(input_file).split("\n").map do |line|
+  color, contains = line.split(' bags contain ')
+  contains = contains.split(', ').map do |item|
+    if (match = item.match(/^(\d+) (.+) bag/))
+      OpenStruct.new count: match[1], color: match[2]
+    end
+  end.compact
+
+  OpenStruct.new color: color, contains: contains #, line: line
+end
+
+color_map = {}
+input.each do |rule|
+  rule.contains.each do |contained|
+    color_map[contained.color] ||= OpenStruct.new color: contained.color, containers: []
+    color_map[contained.color].containers.push rule
+  end
+end
+
+def navigate(color_map, color, answer = [], visited = [])
+  if visited.include?(color)
+    puts "ALREADY VISITED NODE: #{color}"
+  else
+    visited.push color
+    color_map[color]&.containers&.each do |container|
+      answer << container.color unless answer.include?(container.color)
+      navigate color_map, container.color, answer, visited
+    end
+  end
+  answer
+end
+answer = navigate(color_map, 'shiny gold')
+
+puts "answer: #{answer}"
+puts answer.count
---
+++ 007/part_2.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'ostruct'
+
+input_file = ARGV.first || File.join(__dir__, 'input')
+input = File.read(input_file).split("\n").map do |line|
+  color, contains = line.split(' bags contain ')
+  contains = contains.split(', ').map do |item|
+    if (match = item.match(/^(\d+) (.+) bag/))
+      OpenStruct.new count: match[1].to_i, color: match[2]
+    end
+  end.compact
+
+  OpenStruct.new color: color, contains: contains #, line: line
+end
+
+color_map = {}
+input.each do |rule|
+  color_map[rule.color] ||= OpenStruct.new color: rule.color, contains: []
+  rule.contains.each do |contain|
+    color_map[rule.color].contains.push OpenStruct.new(count: contain.count, color: contain.color)
+  end
+end
+
+def navigate(color_map, color)
+  return unless node = color_map[color]
+
+  node.contains.map do |contain|
+    contain.count + contain.count * navigate(color_map, contain.color)
+  end.sum
+end
+answer = navigate(color_map, 'shiny gold')
+
+puts answer
