b7af238a9c7725772bb86f526a6a735c4205ac46
Aldrin Martoq <a@a0.cl>
2020-12-17 01:56:13 -0300

    I am bad at counting.

--- 010/part_2.rb
+++ 010/part_2.rb
@@ -5,35 +5,25 @@ require 'ostruct'
 input_file = ARGV.first || File.join(__dir__, 'input')
 input = File.read(input_file).split("\n").map(&:to_i).sort
 
-delta = {}
-
-input.push input.last + 3
-
 last = 0
-last_diff = 0
-diff_repeat = 0
-repeated = []
-input.each do |n|
+diffs = input.map do |n|
   diff = n - last
-  delta[diff] ||= 0
-  delta[diff] += 1
 
-  if diff == last_diff && diff == 1
-    diff_repeat += 1
-  elsif diff_repeat > 0
-    puts "Repeated: #{diff_repeat}"
-    repeated.push 2**diff_repeat
-    diff_repeat = 0
-  end
-
-  puts "last: #{last} n: #{n} diff: #{diff}"
   last = n
-  last_diff = diff
+  diff
 end
 
-puts delta[1] * delta[3]
+a = diffs.join.split('3').map(&:length).map do |length|
+  case length
+  when 4
+    7
+  when 3
+    4
+  when 2
+    2
+  else
+    1
+  end
+end
 
-puts delta
-puts repeated.reduce(&:*)
-
-require 'pry'; binding.pry
+puts a.reduce(&:*)
